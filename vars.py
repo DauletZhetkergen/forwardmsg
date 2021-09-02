a45eeadf312f7fc6c96dc17b58673dc37176d941
DauletZhetkergen <68519395+DauletZhetkergen@users.noreply.github.com>
2021-09-02 12:17:00 +0500

    updated to channels

--- vars.py
+++ vars.py
@@ -2,40 +2,31 @@ from telethon import TelegramClient, sync, events
 
 
 
-api_id = 'YOUR ID'
-api_hash = 'YOUR HASH'
+api_id = '#'
+api_hash = '#'
 
 chat_ids = 'Your chat id'
 
 client = TelegramClient('session',api_id,api_hash)
-
-
-
-fin_res = []          ##############WRITE IN BRACKETS CHANNELS NAME LIKE ['CHANNELNAME','...'	}
-
-
-
-
-
+client.start()
 #HERE CHECKS TO NEW MESSAGES
-@client.on(events.newmessage.NewMessage(from_users=fin_res))######
+@client.on(events.NewMessage(chats=('New')))
 async def normal_handler1(event):
-	print(event.message)
-	await send_msg(event.message.to_dict()['message'])
+	last_msg = event.message.message
+	specific_words = ['Trade type','Entry zone','Target','Exchange','My apologize']
+	for i in specific_words:
+		if i in last_msg:
+			print("works")
+			await send_msg(event.message.to_dict()['message'])
+
+		else:
+			print("dont")
 #####################
-
-
-
 
 async def send_msg(msg):
 	print(msg)
-	await client.send_message('#', msg)####----WRITE HERE YOUR CHANELLS NAME TO SEND MESSAGE---------#######
+	await client.send_message('Daulet', msg)####----WRITE HERE YOUR CHANELLS NAME TO SEND MESSAGE---------#######
 ###########
 
 
-
-
-
-
-client.start()
 client.run_until_disconnected()
